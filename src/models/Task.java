977fab43542c642a84fe57182c517fe91f5f6cd7
kanazawa.takumi <kanazawa.takumi@e-jan.co.jp>
2019-04-15 17:37:04 +0900

    Modify DTO

--- src/models/Task.java
+++ src/models/Task.java
@@ -7,9 +7,17 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
+@NamedQueries({
+    @NamedQuery(
+            name = "getAllTasks",
+            query = "SELECT t FROM Task AS t ORDER BY t.id DESC"
+            )
+})
 @Table(name = "tasks")
 public class Task {
     @Id
@@ -23,7 +31,7 @@ public class Task {
     @Column(name = "created_at", nullable = false)
     private Timestamp created_at;
 
-    @Column(name = "created_at", nullable = false)
+    @Column(name = "updated_at", nullable = false)
     private Timestamp updated_at;
 
     public Integer getId() {
